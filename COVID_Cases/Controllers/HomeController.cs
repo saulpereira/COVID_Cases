c0aa60ae189aae3d55a5785d873f6701e25e2e75
Saul Pereira <saul_pereira@hotmail.com>
2021-07-12 08:13:03 -0600

    Get Provinces Added

--- COVID_Cases/Controllers/HomeController.cs
+++ COVID_Cases/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -49,8 +50,9 @@ namespace COVID_Cases.Controllers
                 countries = JsonConvert.DeserializeObject<Countries>(resultString, settings);
 
                 model.dataCountries =  this.GetTopCountries(countries);
-                //this.GetTopCountries(countries);
             }
+
+            GetProvincesAsync("USA");
 
 
             return View(model);
@@ -81,11 +83,7 @@ namespace COVID_Cases.Controllers
 
             List<CountriesSelected> lista = new List<CountriesSelected>();
             CountriesSelected countries1 = new CountriesSelected();
-            string search = "US";
-
-            //lista = countries.data.OrderByDescending(x => x.confirmed).Take(10).ToList();
-
-            //var countries2 = countries.data
+           
             var countries2 = countries.data
                 .GroupBy(y => y.Region.iso)
                 .OrderByDescending(group => group.Sum(c => c.confirmed))
@@ -101,24 +99,57 @@ namespace COVID_Cases.Controllers
             return lista;
         }
 
-       
-
-        
-
-        
-
-        public ActionResult About()
+        //public async Task<JsonResult> GetProvincesAsync(string ProvinceId)
+        public void GetProvincesAsync(string ProvinceId)
         {
-            ViewBag.Message = "Your application description page.";
+            HttpClient cliente = new HttpClient();
+            cliente.BaseAddress = new Uri("https://covid-19-statistics.p.rapidapi.com/");
+            cliente.DefaultRequestHeaders.Add("x-rapidapi-key", "ccc4385e04mshf4ed1ca42485851p15dd54jsn577c8b2c68c7");
+            cliente.DefaultRequestHeaders.Add("x-rapidapi-host", "covid-19-statistics.p.rapidapi.com");
 
-            return View();
+            UriBuilder builder = new UriBuilder("https://covid-19-statistics.p.rapidapi.com/reports");
+            builder.Query = "iso=" + ProvinceId;
+            RegionsViewModel model = new RegionsViewModel();
+
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+
+            //var request = cliente.GetAsync("reports").Result;
+            var request = cliente.GetAsync(builder.Uri).Result;
+            Countries countries = new Countries();
+
+            if (request.IsSuccessStatusCode)
+            {
+                var resultString = request.Content.ReadAsStringAsync().Result;
+                countries = JsonConvert.DeserializeObject<Countries>(resultString, settings);
+
+                model.dataCountries = this.GetTopProvinces(countries);
+               
+            }
         }
 
-        public ActionResult Contact()
+        public IEnumerable<CountriesSelected> GetTopProvinces(Countries countries)
         {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+            List<CountriesSelected> lista = new List<CountriesSelected>();
+            CountriesSelected countries1 = new CountriesSelected();
+
+            var countries2 = countries.data
+                .GroupBy(y => y.Region.province)
+                .OrderByDescending(group => group.Sum(c => c.confirmed))
+                .Take(10)
+                .Select(group => new { Name = group.Key, Cases = group.Sum(a => a.confirmed), Deaths = group.Sum(b => b.deaths) })
+                .ToList();
+
+            foreach (var item in countries2)
+            {
+                lista.Add(new CountriesSelected { Name = item.Name, cases = item.Cases, deaths = item.Deaths });
+            }
+
+            return lista;
         }
     }
 }
